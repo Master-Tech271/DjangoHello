8022d883c580ce8da9943f8f69172652f6cbbcb9
Master-Tech271 <umar17605@gmail.com>
2020-04-05 21:45:53 +0530

    first django app

---
+++ Hello/home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+def index(request):
+    return render(request, 'index.html')
+def contact(request):
+    #insert the form data
+    if request.method == 'POST':
+         name = request.POST.get('name')
+         email = request.POST.get('email')
+         desc = request.POST.get('desc')
+         phone = request.POST.get('phone')
+         contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+         contact.save()
+         messages.success(request, 'Your Form has been submited!..')
+    return render(request, 'contact.html')
